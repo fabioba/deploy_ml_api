c99b3ec64c148283a6a7425722ceb39682b959a3
fabiobarbazza <fabio.barbazza@dazn.com>
2022-01-30 22:00:40 +0100

    fix: add packages to requirements and setup

--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(
    packages=find_packages(),
    install_requires=['joblib==1.0.1',
                     'pandas==1.3.5',
-                    'pytest==6.2.5'] #external packages as dependencies
+                    'pytest==6.2.5',
+                    'statsmodels==0.13.1',
+                    'sklearn==0.0'] #external packages as dependencies
 )
