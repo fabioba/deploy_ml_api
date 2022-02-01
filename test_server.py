7465ebfda5f2e86ced4891937cc997912b762975
fabiobarbazza <fabio.barbazza@dazn.com>
2022-02-01 12:04:05 +0100

    fix: update model

--- test_server.py
+++ test_server.py
@@ -38,7 +38,7 @@ def test_api_locally_get_root():
 
 def test_run_inference_negative():
     cens1={"age":39, "workclass":'State-gov',
-    "fnlgt":77516, "education":'Bachelors',"education_num":8, "marital_status":'Never-married', "occupation":'Adm-clerical',"relationship":'Not-in-family', "race":'White', "sex":'Male',"capital_gain":2174,"capital_loss":0,"hours_per_week":40, "native_country":'United-States',"salary":'<=50K'}
+    "fnlgt":77516, "education":'Bachelors',"education_num":8, "marital_status":'Never-married', "occupation":'Adm-clerical',"relationship":'Not-in-family', "race":'White', "sex":'Female',"capital_gain":2174,"capital_loss":0,"hours_per_week":40, "native_country":'United-States',"salary":'<=50K'}
     
     r=client.post("/inference/",json=cens1)
     assert r.status_code == 200
@@ -48,12 +48,12 @@ def test_run_inference_negative():
     assert content['preds']['0']==0
 
 def test_run_inference_positive():
-    cens1={"age":31, "workclass":'Private',
-    "fnlgt":292516, "education":'Masters',"education_num":14, "marital_status":'Divorced', "occupation":'Exec-managerial',"relationship":'Unmarried', "race":'White', "sex":'Female',"capital_gain":14084,"capital_loss":0,"hours_per_week":60, "native_country":'United-States',"salary":'>50K'}
+    cens1={"age":31, "workclass":'Self-emp-not-inc',
+    "fnlgt":45781, "education":'HS-grad',"education_num":14, "marital_status":'Married-civ-spouse', "occupation":'Tech-support',"relationship":'Husband', "race":'White', "sex":'Male',"capital_gain":14084,"capital_loss":0,"hours_per_week":50, "native_country":'United-States',"salary":'>50K'}
 
     r=client.post("/inference/",json=cens1)
     assert r.status_code == 200
 
     content=r.json()
 
-    assert content['preds']['0'] ==0
+    assert content['preds']['0'] ==1
